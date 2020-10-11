b8cc603ee96035b5a446ecffae3d60cf935fcacc
Ricardo Barrera <rbarrera87@gmail.com>
2020-10-11 16:23:24 -0700

    First steps for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,7 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!
 
-  before_action :set_question, only: [:index]
+  before_action :set_question, only: [:index, :new, :create]
 
   layout 'teachers'
 
@@ -9,7 +9,26 @@ class AnswersController < ApplicationController
     @answers = @question.answers
   end
 
+  def new
+    @answer = @question.answers.new
+  end
+
+  def create
+    @answer = @question.answers.new(answer_params)
+    respond_to do |format|
+      if @answer.save
+        format.html { redirect_to question_answers_url, notice: 'Respuesta creada correctamente.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
   private
+
+  def answer_params
+    params.require(:answer).permit(:text, :correct)
+  end
 
   def set_question
     @question = Question.find(params[:question_id])
